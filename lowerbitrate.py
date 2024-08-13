7f058c1327ddf8b138c235a543711c5c3a6a5d76
Louis Volant <louis.volant@kindredgroup.com>
2024-08-13 21:06:29 +0200

    Fixed issue for when bit_rate is not available + Improved logging showing space percentage saved + improved process by not doing anything if not needed

--- lowerbitrate.py
+++ lowerbitrate.py
@@ -8,13 +8,13 @@ from pydub.utils import mediainfo
 
 TARGET_BITRATE = "128k"
 
-
 # README
 # execute with
-# $ lowerbitrate % pip install pydub
-# $ lowerbitrate % python3 lowerbitrate.py
-
-
+# python3 -m venv myenv
+# source myenv/bin/activate
+# pip install pydub 
+# python3 lowerbitrate.py
+# Once finished, simply desactivate the virtual environment using "deactivate"
 
 
 def get_file_size(file_path):
@@ -49,17 +49,29 @@ def human_readable_number(num):
         return f"{num:.1f}{suffixes[suffix_index]}"
 
 def handleMp3File(inputFilePath):
-    original_bitrate = mediainfo(inputFilePath)['bit_rate']
+    original_bitrate = 0
+    if 'bit_rate' in mediainfo(inputFilePath):
+	    original_bitrate = float(mediainfo(inputFilePath)['bit_rate'])
+    
+    original_file_size = get_file_size(inputFilePath)
     logging.info('Original Bitrate: {0}. Original size : {1}'
-                 .format(human_readable_number(original_bitrate), human_readable_number(get_file_size(inputFilePath))))
+                 .format(human_readable_number(original_bitrate), human_readable_number(original_file_size)))
 
-    sound = AudioSegment.from_file(inputFilePath)
-    sound.export(inputFilePath, format="mp3", bitrate=TARGET_BITRATE)
+    if original_bitrate > 135000:
+        sound = AudioSegment.from_file(inputFilePath)
+        sound.export(inputFilePath, format="mp3", bitrate=TARGET_BITRATE)
 
-    updated_bitrate = mediainfo(inputFilePath)['bit_rate']
-    logging.info('Updated Bitrate: {0}. Updated size : {1}'
-                 .format(human_readable_number(updated_bitrate), human_readable_number(get_file_size(inputFilePath))))
+        updated_bitrate = mediainfo(inputFilePath)['bit_rate']
+        updated_file_size = get_file_size(inputFilePath)
+        
+        percentage = (updated_file_size / original_file_size) * 100
+        percentage_str = f"{percentage:.2f}%"
 
+        logging.info('Updated Bitrate: {0}. Updated size : {1} ({2})'
+                    .format(human_readable_number(updated_bitrate), human_readable_number(updated_file_size), percentage_str))
+    else:
+        logging.info('Bitrate: {0} already close or under target bitrate: {1}. Doing nothing.'
+            .format(human_readable_number(original_bitrate), TARGET_BITRATE))
 
 
 def main():
